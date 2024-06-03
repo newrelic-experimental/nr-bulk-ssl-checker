4026081747d7ef104217b81016e3e053a5b992fa
James Buchanan <jbuchanan@newrelic.com>
2024-06-03 14:35:32 +0100

    Moved auth config to be target specific. Some other error message tidying

--- terraform/modules/sslchecker/modules/sslminion/src/header.js
+++ terraform/modules/sslchecker/modules/sslminion/src/header.js
@@ -7,7 +7,6 @@ const WARNING_THRESHOLD = 28  // number of days left to trigger warning state
 
 const BATCH_SIZE = 50; // number of checks to run at once
 const DEFAULT_TIMEOUT = 5000; //TLS handshake timeout default (can be overdiden in config)
-const REJECTUNAUTHORIZED = false // If not false, the server certificate is verified against the list of supplied CAs. An 'error' event is emitted if verification fails; err.code contains the OpenSSL error code. Default: true. https://nodejs.org/docs/latest/api/tls.html#tlssocketrenegotiateoptions-callback 
 
 let RUNNING_LOCALLY = false; // for testing purposes
 
